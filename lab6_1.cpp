415eade7ffd1bb9663cdbdbb631c82bbcea28f48
007lnwza <phanudet_sueaphueak@cmu.ac.th>
2022-12-09 15:20:23 +0700

    sed la 1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    int number=1,odd=0,even=0;
+    while(number!=0){
+        cout << "Enter an integer: ";
+        cin >>number;
+     if(number%2==0){
+            even++;
+        }else{
+            odd++;
+        }
+}
     cout << "#Even numbers = ";
+    cout << even-1<<endl;
     cout << "#Odd numbers = ";
+    cout << odd<<endl;
     return 0;
 }
